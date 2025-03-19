8a7305c9755abd334192be8136a8a5f9dfd92573
neetibut <neetibut@gmail.com>
2025-03-19 22:59:47 +0700

    repo(setup): installed and tested TailwindCSS, cleaned up unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex justify-center bg-blue-950">
+      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+          React App Starter
+        </h1>
+        <section className="w-full p-5 bg-amber-100 flex">
+          <ul className="list-inside list-disc flex-1">
+            <span className="font-semibold">Tech Stack:</span>
+            <li>Vite</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
